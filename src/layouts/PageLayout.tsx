3ad07dac9fcc989c790901a4f98610099b06b88c
charnchai36 <charnchai36@outlook.com>
2022-01-13 09:43:28 +0900

    layout components

---
+++ src/layouts/PageLayout.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { Header, Footer, BetaWarningBanner, CustomModal } from 'components';
+import Background from './Background';
+import { useIsProMode } from 'state/application/hooks';
+import { useActiveWeb3React } from 'hooks';
+import { useArcxAnalytics } from '@arcxmoney/analytics';
+
+export interface PageLayoutProps {
+  children: any;
+  name?: string;
+}
+
+const PageLayout: React.FC<PageLayoutProps> = ({ children, name }) => {
+  const history = useHistory();
+  const { chainId, account } = useActiveWeb3React();
+  const arcxSDK = useArcxAnalytics();
+  const { isProMode, updateIsProMode } = useIsProMode();
+  const [openPassModal, setOpenPassModal] = useState(false);
+  const getPageWrapperClassName = () => {
+    if (isProMode) {
+      return '';
+    }
+    return name == 'prdt' ? 'pageWrapper-no-max' : 'pageWrapper';
+  };
+  useEffect(() => {
+    const unlisten = history.listen((location) => {
+      updateIsProMode(false);
+    });
+    return function cleanup() {
+      unlisten();
+    };
+  }, [history, updateIsProMode]);
+
+  useEffect(() => {
+    if (
+      window.location.host !== 'quickswap.exchange' &&
+      window.location.host !== 'beta.quickswap.exchange' &&
+      window.location.host !== 'localhost:3000'
+    ) {
+      setOpenPassModal(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (arcxSDK && account && chainId) {
+        await arcxSDK.connectWallet({ account, chain: chainId });
+      }
+    })();
+  }, [account, chainId, arcxSDK]);
+
+  const PasswordModal = () => {
+    const [devPass, setDevPass] = useState('');
+    const confirmPassword = () => {
+      if (devPass === 'devPass') {
+        setOpenPassModal(false);
+      }
+    };
+    return (
+      <CustomModal open={openPassModal} onClose={confirmPassword}>
+        <Box className='devPassModal'>
+          <p>Please input password to access dev site.</p>
+          <input
+            type='password'
+            value={devPass}
+            onChange={(e) => {
+              setDevPass(e.target.value);
+            }}
+          />
+          <Box textAlign='right'>
+            <Button onClick={confirmPassword}>Confirm</Button>
+          </Box>
+        </Box>
+      </CustomModal>
+    );
+  };
+
+  const showBetaBanner = false;
+
+  return (
+    <Box className='page'>
+      {openPassModal && <PasswordModal />}
+      {showBetaBanner && <BetaWarningBanner />}
+      <Header />
+      {!isProMode && <Background fallback={false} />}
+      <Box className={getPageWrapperClassName()}>{children}</Box>
+      <Footer />
+    </Box>
+  );
+};
+
+export default PageLayout;
